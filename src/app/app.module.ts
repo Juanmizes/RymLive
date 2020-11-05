8c6c1f58a1a421a5847e21f3beae28fa5490b8f0
Juan Manuel Izquierdo <omgthistormenta@gmail.com>
2020-11-05 02:23:54 +0100

    primera version funcional del reproductor y algunas canciones locales para hacer pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
+
+import { PlyrModule } from 'ngx-plyr';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { MusicPlayerComponent } from './music-player/music-player.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatCardModule,
+    PlyrModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -12,4 +12,30 @@ export class MusicPlayerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  player: Plyr;
+
+  audioSources = [
+    {
+      src: '../../assets/music/1 - J Balvin - Brillo.mp3',
+      type: 'audio/mp3',
+    },
+  ];
+
+  played(event: Plyr.PlyrEvent) {
+    console.log('played', event);
+  }
+
+  play(): void {
+    this.player.play(); // or this.plyr.player.play()
+  }
+
+  pause(): void {
+    this.player.pause(); // or this.plyr.player.play()
+  }
+
+  stop(): void {
+    this.player.stop(); // or this.plyr.player.stop()
+  }
+
 }
+
