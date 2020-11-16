c53150707f403aedf38c6b2626f1bd402d163720
asdrúbal sánchez <asdrubal.sanchez.dam@gmail.com>
2020-11-16 12:26:48 +0100

    Actualización del perfil de usuario e onstalación jQuery and JQueryTypes

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,17 +6,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-showChangeImage: boolean;
+  settingsAppear: boolean;
+  oldValues: string;
+  newValues: string;
+  imgRute: string;
+  imageDefault: string;
+  imageActually: string;
+
+  showTableHeader: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.showChangeImage = false;
+    this.oldValues = document.getElementById('temporal').innerHTML;
+
+    this.imgRute = "../../assets/img/";
+    this.imageDefault = "account_circle-24px.svg";
+    this.imageActually = this.imageDefault;
+
+    this.settingsAppear = false;
+    this.showTableHeader = false;
+
+    this.playMusic();
   }
 
-  changeImage(){
-    this.showChangeImage = true;
-    console.log("FUNCIONA");
+  inSettings(){
+    this.settingsAppear = true;
+    console.log("In settings");
+  }
+
+  saveChanges(){
+    //Change Biography
+    this.newValues = (document.getElementById('biography-input') as HTMLInputElement).value;
+    this.oldValues = this.newValues;
+
+    //Change Image
+    const input = <HTMLInputElement>document.getElementById('customFile');
+    if(input.files && input.files[0]){
+      console.log("File selected : ", input.files[0]);
+      this.imageActually =  input.files[0].name;
+      
+    }else 
+      console.log("No file selected " + input.files[0]);
+
+    this.settingsAppear = false;
+    console.log("Settings disappear");
+  }
+
+  loadTable(){
+    this.showTableHeader = true;
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', '../../assets/json/mySongTable.json', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+
+      if(this.readyState == 4 && this.status == 200){
+
+        let datos = JSON.parse(this.responseText);
+        let res = document.querySelector('#tableBody');
+        res.innerHTML='';
+
+        for(let item of datos){
+          res.innerHTML += `
+          <tr>
+            <th scope="row">${item.id}</th>
+            <td>${item.soungName}</td>
+            <td>${item.autor}</td>
+            <td>${item.time}</td>
+            <td><div id="musicPlayer-icon"><button type="button" class="btn btn-outline-dark btn-sm" (click)="playMusic()">Play</button></div></td>
+          </tr>
+          `
+        }
+      }
+    }
+  }
+
+  playMusic(){  
+
   }
 
 }
