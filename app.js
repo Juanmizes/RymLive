00b70be7697c2ad120fc068776d4345aa25c97aa
Juanma <omgthistormenta@gmail.com>
2020-12-20 15:17:16 +0100

    cors header request

--- app.js
+++ app.js
@@ -19,9 +19,11 @@ api.use(express.json());
 // CORS (permitir peticiones desde el front end)
 api.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Request-Headers');
+    // res.header('Access-Control-Request-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('');
     next();
 });
 
