b4f56e02e7744c282e08835db9e6ffff07387661
asdrúbal sánchez <asdrubal.sanchez.dam@gmail.com>
2021-01-08 15:43:49 +0100

    Remodelado del reproductor nuevo tanto para pc como para móviles

--- dist/RymLive/polyfills.js
+++ dist/RymLive/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Juanma\Desktop\RymLiveFront\RymLive\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\Asdru\OneDrive\Escritorio\RymLive\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
