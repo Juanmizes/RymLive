3977ba443f6c637e9d9ea786f8b82109e76396e3
Juanma <omgthistormenta@gmail.com>
2021-01-08 14:00:01 +0100

    Varios archivos

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,8 @@ router.put('/profile/update',userController.tokenMiddleware, userController.upda
 router.get('/profile',userController.tokenMiddleware,userController.getOwnUser);
 router.get('/profile/user/:id', userController.getUser);
 router.get('/search/:search', userController.tokenMiddleware, userController.searchUser)
-router.put('/profile/updateImage', uploadImages ,userController.uploadImage);
-router.get('/getImage/:image', userController.tokenMiddleware, userController.getImage);
+router.put('/profile/updateImage', userController.tokenMiddleware, uploadImages ,userController.uploadImage);
+router.get('/getImage/:image', userController.getImage);
 
 //Rutas Musica usuario
 router.post('/uploadSong',userController.tokenMiddleware, uploadSongs ,userController.uploadSong);
